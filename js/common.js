edfae1454234f8a6c5e2ca2524ff1732f0eeecba
feicm <feicm66@gmail.com>
2014-11-27 16:42:42 +0800

    add wfcx result mult tab

--- js/common.js
+++ js/common.js
@@ -309,13 +309,13 @@ var App = (function () {
                             _defaultClass && me.removeClass(_defaultClass);
                         }
                         me.addClass(_activeClass);
-                        currentTabContent.show('linner');
+                        currentTabContent.show();
                         tabItem.each(function (index) {
                             var me = $(this);
                             if ( me.attr('data-for') !== dataFor ) {
                                 me.removeClass(_activeClass);
                                 _defaultClass && me.addClass(_defaultClass);
-                                $('#' + me.attr('data-for')).hide('linner');
+                                $('#' + me.attr('data-for')).hide();
                             }
                         });
                     });
