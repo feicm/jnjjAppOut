ec636f00349e68a0614c79a528859aad31a00fd8
feicm <feicm66@gmail.com>
2014-12-09 16:20:06 +0800

    optimize violation select

--- js/violationmsg.js
+++ js/violationmsg.js
@@ -16,12 +16,12 @@ $(function () {
         + '/jnpublic/config/html/resultlisttab.jsp';//查询结果页地址(带选项卡)
     var clxcSubmit = $('#clxc_submit');
     var jzcxSubmit = $('#jzcx_submit');
-    clxcSubmit.on('click', clxcListener);
-    jzcxSubmit.on('click', jzcxListener);
+    /*clxcSubmit.on('click', clxcListener);
+     jzcxSubmit.on('click', jzcxListener);*/
     //按车辆 提交事件
     function clxcListener() {
         var ip_hphm = document.getElementById('hphm');
-        var index=ip_hphm.selectedIndex;
+        var index = ip_hphm.selectedIndex;
         var hphm = ip_hphm.options[index].text;
         var hpzl = ip_hphm.options[index].getAttribute('data-type');
         var jkbj;
@@ -32,15 +32,12 @@ $(function () {
          "carid"   : hphm
          };*/
         clxcSubmit.off('click');
-        if ( hpzl !== '未绑定' ) {
-            var params = '#mode=wf_car@cartype=' + hpzl + '@carid=' + hphm + '@jkbj=' + jkbj;
-            window.open(infoPageUrl + params);//通过url hash传参
-            clxcSubmit.on('click', clxcListener);
-        } else {
-            alert('未绑定车辆！');
-            clxcSubmit.on('click', clxcListener);
-        }
+        var params = '#mode=wf_car@cartype=' + hpzl + '@carid=' + hphm + '@jkbj=' + jkbj;
+        window.open(infoPageUrl + params);//通过url hash传参
+        clxcSubmit.on('click', clxcListener);
+
     }
+
     //按驾照查询提交事件
     function jzcxListener() {
         var ip_jzxm = $('#jzxm');
@@ -48,22 +45,32 @@ $(function () {
         var jkbj;
         jzcxSubmit.off('click');
         $('#nodo02').prop('checked') ? jkbj = 0 : jkbj = '';
-        if ( licenseid !== '未绑定' ) {
-            var params = '#mode=wf_card@licenseid=' + licenseid + '@jkbj=' + jkbj;
-            window.open(infoTabPageUrl + params);//通过url hash传参
-            jzcxSubmit.on('click', jzcxListener);
-        } else {
-            alert('未绑定驾照！');
-            jzcxSubmit.on('click', jzcxListener);
+        var params = '#mode=wf_card@licenseid=' + licenseid + '@jkbj=' + jkbj;
+        window.open(infoTabPageUrl + params);//通过url hash传参
+        jzcxSubmit.on('click', jzcxListener);
+    }
+
+    initSubmitBtn();
+    //init select
+    function initSubmitBtn() {
+        clxcSubmit.addClass('ui_btn_01_disable');
+        jzcxSubmit.addClass('ui_btn_01_disable');
+    }
+
+    //bindEvent
+    function bindEvent(input, btn, listener) {
+        var inputVal = input.val();
+        var _btn = btn;
+        var _listener = listener;
+        if ( inputVal !== '未绑定' ) {
+            App.UI('buttonHover', {//添加按钮点击效果
+                "dom"           : _btn,
+                "hoverClassName": 'ui_btn_01_hover'
+            });
+            _btn.on('click', _listener);
         }
     }
-    //bindEvent
-    function bindEvent(input,btn,listener){
-        return;
-         var _btn=btn;
-        var _listener=listener;
-        _btn.on('click',_listener);
-    }
+
     /*
      * --------------------页面效果------------------------
      * */
@@ -76,23 +83,15 @@ $(function () {
         "url"   : carlistRequestUrl,
         "data"  : {'register': userName, 'axisFlag': true},
         "module": 'car'
-    },function(){
-         bindEvent(clxcSubmit,clxcListener);
+    }, function () {
+        bindEvent($('#hphm'), clxcSubmit, clxcListener);
     });
     App.UI('select', {
         "dom"   : $('#jzxm'),
         "url"   : cardlistRequestUrl,
         "data"  : {'register': userName, 'axisFlag': true},
         "module": 'license'
-    },function(){
-        bindEvent($('#jzxm'),jzcxSubmit,jzcxListener);
-    });
-    App.UI('buttonHover', {//添加按钮点击效果
-        "dom"           : clxcSubmit,
-        "hoverClassName": 'ui_btn_01_hover'
-    });
-    App.UI('buttonHover', {//添加按钮点击效果
-        "dom"           : jzcxSubmit,
-        "hoverClassName": 'ui_btn_01_hover'
+    }, function () {
+        bindEvent($('#jzxm'), jzcxSubmit, jzcxListener);
     });
 });
