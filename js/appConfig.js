5c37cc457fac63ab7c7d3b11f0ec5e1100463263
feicm <feicm66@gmail.com>
2015-01-30 15:26:25 +0800

    adjust carType adapte

--- js/appConfig.js
+++ js/appConfig.js
@@ -144,7 +144,7 @@ jnjjApp.footbarDatas = {
                             "name"      : "违法信息",
                             "iconpos"   : "top",//图标位置——top|bottom|left|right|notxt上、下、左、右、无文字
                             "clickEvent": "",
-                            "url"       : PreQuestUrl + "/jnpublic/config/html/violation.jsp&@@webViewPageId=" + PageId_lv01 + Wisp.CommenFunc.getRandom() + "@@"
+                            "url"       : "config/html/violation.html&@@webViewPageId=" + PageId_lv01 + Wisp.CommenFunc.getRandom() + "@@"
                         },
                         {
                             "btnType"   : "icobtn",//带图标按钮 ;txtbtn,纯文本
